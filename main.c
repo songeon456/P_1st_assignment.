e4c5c823228114af7b8dbd0463775b4a8d96e147
songeon456 <31638936+songeon456@users.noreply.github.com>
2017-09-10 19:18:37 +0900

    P_1st_assignment.

---
+++ main.c
@@ -0,0 +1,19 @@
+//
+//  main.c
+//  1st_assignment
+//
+//  Created by SonGeonWoo on 2017. 9. 10..
+//  Copyright © 2017년 SonGeonWoo. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    
+    printf("2013301033\n");
+    printf("손건우\n");
+    printf("01050986961\n");
+    printf("songeon456\n");
+    
+    return 0;
+}
